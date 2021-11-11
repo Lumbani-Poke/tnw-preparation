b43aab63b3a7c835d884fe6d287e69f71607b0eb
Lumbani Sowoya <lumbanisowoya@my-macbook.local>
2021-11-11 12:43:15 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+/***********----LEFT SLIDE-----***********/
+
+ /*function openNav() {
+    document.getElementById("menu").classList.toggle('active');
+  }
+  function closeNav() {
+    document.getElementById("menu").classList.toggle('active');
+  }*/
+/***********----RIGHT SLIDE-----***********/
+
+  /*function Slide() {
+    document.getElementById("center-part").classList.toggle('slide');
+  } */
